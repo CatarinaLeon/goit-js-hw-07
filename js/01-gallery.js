8e61095213e70f3efe18427f0012aafc52357cb8
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2021-11-11 20:01:07 +0200

    2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.addEventListener('click', onSelectImage);
+
+
+// Подключение библиотеки basicLightbox
+
+const instance = basicLightbox.create(`<img src="" />`, {
+  onShow: () => {
+    window.addEventListener('keydown', keydownEscape);
+  },
+  onClose: () => {
+    window.removeEventListener('keydown', keydownEscape);
+  },
+});
+
+
+
+// Создание и рендер разметки по массиву данных galleryItems
+
+function renderImages() {
+    const markup = galleryItems.map(
+        ({ preview, original, description }) =>
+            `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+        .join('');
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+renderImages();
+
+// получение url большого изображения
+function onSelectImage(event) {
+    event.preventDefault();
+    instance.element().querySelector('img').src = event.target.dataset.source;
+    instance.show();
+}
+
+function keydownEscape(event) {
+  console.log(event);
+  if (event.key === 'Escape') {
+    instance.close();
+    return;
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+// Создание и рендер разметки по массиву данных galleryItems
+
+function renderImages() {
+    const markup = galleryItems.map(
+        ({ preview, original, description }) =>
+            `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+        .join('');
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
+renderImages();
+
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены
+
+new SimpleLightbox('.gallery .gallery__item', { /* options */ captionsData:'alt', captionDelay:250 });
